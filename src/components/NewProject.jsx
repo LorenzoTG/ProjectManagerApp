ce94640d3709520ce3d97696244f10235d31b3cc
Lorenzo Gaviglio <lorenzotgaviglio97@gmail.com>
2024-02-28 18:11:59 +0100

    Added modal component

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onNewProject }) {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +13,14 @@ export default function NewProject({ onNewProject }) {
     const projectDescription = description.current.value;
     const projectDueDate = dueDate.current.value;
 
-    // validations
+    if (
+      projectTitle.trim() === "" ||
+      projectDescription.trim() === "" ||
+      projectDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onNewProject({
       title: projectTitle,
@@ -21,32 +30,39 @@ export default function NewProject({ onNewProject }) {
   };
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex gap-4 justify-end items-center my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            CANCEL
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={saveDataHandler}
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            SAVE
-          </button>
-        </li>
-      </menu>
-      <section>
-        <Input ref={title} labelText="Title" textArea={false} type="text" />
-        <Input ref={description} labelText="Description" textArea={true} />
-        <Input
-          ref={dueDate}
-          labelText="Due date"
-          textArea={false}
-          type="date"
-        />
-      </section>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>Invalid Input</h2>
+        <p>Oops... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex gap-4 justify-end items-center my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              CANCEL
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={saveDataHandler}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              SAVE
+            </button>
+          </li>
+        </menu>
+        <section>
+          <Input ref={title} labelText="Title" textArea={false} type="text" />
+          <Input ref={description} labelText="Description" textArea={true} />
+          <Input
+            ref={dueDate}
+            labelText="Due date"
+            textArea={false}
+            type="date"
+          />
+        </section>
+      </div>
+    </>
   );
 }
